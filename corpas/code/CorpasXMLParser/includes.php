a8437aff34720b66a90950097817d2bc91eacf83
steviebarrett <mail@steviebarrett.com>
2020-01-21 11:27:52 +0000

    Generated XML files added to outXML/

--- corpas/code/CorpasXMLParser/includes.php
+++ corpas/code/CorpasXMLParser/includes.php
@@ -1,8 +1,8 @@
 <?php
 
 //constants
-define(INPUT_FILEPATH, "../editableTXT/");
-define(OUTPUT_FILEPATH, "../outXML/");
+define(INPUT_FILEPATH, "../../editableTXT/");
+define(OUTPUT_FILEPATH, "../../outXML/");
 
 /* autoload classes anonymus function */
 spl_autoload_register(function ($class) {
