5cba590b6270a672aa5890c1293d62ce18c2cde8
steviebarrett <mail@steviebarrett.com>
2020-01-22 10:23:55 +0000

    - Added emdash handling to CorpasXMLParser Tokeniser - Numerous updates (ongoing) to CorpasXMLLemmatiser including Database class to work with lemmas DB

--- corpas/code/CorpasXMLLemmatiser/classes/Lemmatiser.php
+++ corpas/code/CorpasXMLLemmatiser/classes/Lemmatiser.php
@@ -4,33 +4,53 @@
 class Lemmatiser
 {
   private $_inputXml;
-  private $_dictEntries;
+  private $_dbh;
 
   public function __construct($xml) {
     $this->_inputXml = $xml;
-    //open JSON lemmas file
-    $json = file_get_contents("./json/lemmas.json");
-    $this->_dictEntries = json_decode($json, TRUE);
+    //connect to the database
+    $this->_dbh = Database::getDatabaseHandle(DB_NAME);
   }
 
   public function getProcessedXml() {
-    $outputXml = "";
-
     $text = new SimpleXMLElement($this->_inputXml);
+    if (isset($text->text)) {             //deal with texts within texts
+      $xml = "";
+      foreach ($text->text as $subtext) {
+        $xml .= "\n" . $this->_processTextXml($subtext);
+      }
+      return $xml;
+    } else {
+      return $this->_processTextXml($text);    //single text
+    }
+  }
+
+  private function _processTextXml($text) {
     foreach ($text->p as $p) {
       if (isset($p->w)) {
         foreach ($p->w as $word) {
           $wordform = (string)$word;
-  //return var_dump($this->_dictEntries);
-          foreach ($this->_dictEntries as $entry => $e) {
-            if ($wordform == $entry->wordform) {
-              $outputXml .= "\n{$wordform} : " . $e["lemma"];
-            }
-          }
-
+          $lemmas = $this->_getLemmas($wordform);
+          $lemmasGlued = implode(' ', $lemmas);
+          $word["lemma"] = $lemmasGlued;
         }
       }
     }
-    return $outputXml;
+    return $text->asXML();
+  }
+
+  private function _getLemmas($wordform) {
+    $lemmas = array();
+    //TODO: ask MM if we want to remove duplicates using DISTINCT
+    $query = <<<SQL
+        SELECT DISTINCT lemma FROM lemmas WHERE wordform = :wordform
+SQL;
+    $sth = $this->_dbh->prepare($query);
+    $sth->execute(array(":wordform" => $wordform));
+    $results = $sth->fetchAll();
+    foreach ($results as $result) {
+      $lemmas[] = $result["lemma"];
+    }
+    return $lemmas;
   }
 }
--- corpas/code/CorpasXMLLemmatiser/includes.php
+++ corpas/code/CorpasXMLLemmatiser/includes.php
@@ -3,6 +3,13 @@
 //constants
 define(INPUT_FILEPATH, "./inputFiles/");
 define(OUTPUT_FILEPATH, "./outputFiles/");
+//DB
+define(DB_HOST, "localhost");
+define(DB_NAME, "multidict");
+define(DB_USER, "web");
+define(DB_PASSWORD, "craobh");
+
+//DB_HOST . ";dbname=" . $dbName . ";charset=utf8;", DB_USER, DB_PASSWORD
 
 /* autoload classes anonymus function */
 spl_autoload_register(function ($class) {
