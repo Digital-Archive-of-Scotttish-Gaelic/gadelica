e422ddb23d6f2f97930e7196ee9558020814983d
conmaol <Mark.McConville@glasgow.ac.uk>
2020-03-04 11:49:49 +0000

    stuff

--- corpas/code/index.php
+++ corpas/code/index.php
@@ -1,172 +1,34 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
-    <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"/>
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bPopup/0.11.0/jquery.bpopup.min.js"/>-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Corpas na Gàidhlig</title>
   </head>
   <body>
     <div class="container-fluid">
       <h1>Corpas na Gàidhlig</h1>
       <div class="list-group list-group-flush">
-        <div class="list-group-item list-group-item-action">#1: Somhairle MacGill-Eain: Dàin do Eimhir – 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/I">I</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/II_A_Chiall_s_a_Ghraaidh">II: A’ Chiall ’s a’ Ghràidh</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/III">III</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/IV">IV</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/V">V</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/VI">VI</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/VIII">VIII</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/IX">IX</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/X">X</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XI">XI</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XII">XII</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XIII">XIII</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XIV_Reic_Anama">XIV: Reic Anama</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XV_Trii_Slighean">XV: Trì Slighean</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XVI">XVI</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XVII">XVII</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XVIII_UUrnaigh">XVIII: Ùrnaigh</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XIX">XIX</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XX">XX</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXI">XXI</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXII">XXII</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXIII">XXIII</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXIV">XXIV</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXV">XXV</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXVI">XXVI</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXVII">XXVII</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXVIII_Na_Samhlaidhean">XXVIII: Na Samhlaidhean</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXIX_Coin_is_Madaidhean-allaidh">XXIX: Coin is Madaidhean-allaidh</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXX">XXX</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXXI">XXXI</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXXII">XXXII</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXXIII">XXXIII</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXXIV">XXXIV</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXXV">XXXV</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXXVI">XXXVI</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXXVII">XXXVII</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXXVIII">XXXVIII</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XXXIX">XXXIX</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XL">XL</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XLI">XLI</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XLII_Traaighean">XLII: Tràighean</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XLIII">XLIII</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XLIV">XLIV</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XLV">XLV</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XLVI">XLVI</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XLVII">XLVII</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XLVIII">XLVIII</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/XLIX">XLIX</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/L">L</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/LI">LI</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/LII">LII</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/LIII">LIII</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/LIV">LIV</a>, 
-          <a href="viewText.php?t=1_Daain_do_Eimhir/LV">LV</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/LVI">LVI</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/LVII">LVII</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/LVIII">LVIII</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/LIX">LIX</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/LX">LX</a>,
-          <a href="viewText.php?t=1_Daain_do_Eimhir/Dimitto">Dimitto</a>
-        </div>
-        <div class="list-group-item list-group-item-action">#2: Sgeulachdan Dhòmhnaill Alasdair – 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/1_A_Chaora_Dhubh">A’ Chaora Dhubh</a>, 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/2_A_Chiad_Rong_san_Fhaaradh">A’ Chiad Rong san Fhàradh</a>, 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/3_Am_Preas_Aodaich">Am Preas Aodaich</a>, 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/4_An_Cooirneal">An Còirneal</a>, 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/5_An_Dalta">An Dalta</a>,
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/6_An_Lannsair_Chraobhan">An Lannsair Chraobhan</a>, 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/7_Aonghas_Thuilm">Aonghas Thuilm</a>, 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/8_Ciste_a_Chaiptein">Ciste a’ Chaiptein</a>, 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/9_Cogais_Ghoirt">Cogais Ghoirt</a>, 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/10_Cruaidh-fhortan_Bhriain">Cruaidh-fhortan Bhriain</a>,
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/11_Daa_Chaora_Dhubh">Dà Chaora Dhubh</a>,
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/12_Diioghaltas_Syd_Scot">Dìoghaltas Syd Scot</a>,
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/13_Diiomhaireachd_nan_Dotairean">Dìomhaireachd nan Dotairean</a>,
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/14_Droch_Bhliadhna_san_Sgoil">Droch Bhliadhna san Sgoil</a>,
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/15_Eucoir_agus_Peanasachadh">Eucoir agus Peanasachadh</a>,
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/16_Eudach">Eudach</a>, 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/17_Fear_na_Bainnse">Fear na Bainnse</a>, 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/18_Gaisgeach_Duuthchail">Gaisgeach Dùthchail</a>, 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/19_Iasgach_nan_Giomach">Iasgach nan Giomach</a>, 
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/20_Mata_agus_Lucas">Mata agus Lucas</a>,
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/21_Poosadh_Smart_Ailig">Pòsadh ‘Smart Ailig’</a>,
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/22_Rob_an_Deucoin">Rob an Deucoin</a>,
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/23_Saighead_an_Tighearna">Saighead an Tighearna</a>,
-          <a href="viewText.php?t=2_Sgeulachdan_Dhoomhnaill_Alasdair/24_Toiseach_a_Shoolais">Toiseach a’ Shòlais</a>
-        </div>
-        <div class="list-group-item list-group-item-action">#3: Smuaintean fo Éiseabhal – 
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Am_Baard">Am Bàrd</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/An_Rocaid">An Rocaid</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/An_t-Eun_Fuadain">An t-Eun Fuadain</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Cha_Mhoor_tha_ri_AAireamh">Cha Mhór tha ri Àireamh</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Caochladh">Caochladh</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Cha_Till_an_OOige">Cha Till an Òige</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Baagh_Hartabhagh">Bàgh Hartabhagh</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Coomhradh_an_Eilthirich_ris_an_Stuaidh">Còmhradh an Eilthirich ris an Stuaidh</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Cumha">Cumha</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Dealbh_Camilla">Dealbh Camilla</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Don_Bhean_Phoosta_Jacqueline_Marsh">Don Bhean Phòsta Jacqueline Marsh</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Do_Mhaairi_NicillEathain_Griomsaigh">Do Mhàiri NicillEathain Griomsaigh</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Faailt_an_t-Sagairt_OOig">Fàilt an t-Sagairt Òig</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Don_Tee_Rinn_Tighinn">Don Té Rinn Tighinn gam Fheòraich Dhachaigh</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Eich_EEirisgeigh">Eich Éirisgeigh</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/EEiseabhal">Éiseabhal</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Eilean_Uibhist">Eilean Uibhist</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/EEiseabhal_Stuucach">Éiseabhal Stùcach</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Tiir_nan_Raon">Tìr nan Raon</a>,
-          Mìosan na Bliadhna 
-          (<a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Am_Faoilteach">Am Faoilteach</a>, 
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/An_Gearran">An Gearran</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Am_Maart">Am Màrt</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/A_Ghiblin">A’ Ghiblin</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Am_Maaigh">Am Màigh</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/An_t-OOgmhios">An t-Ògmhios</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/An_t-Iuchar">An t-Iuchar</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/An_Luunastal">An Lùnastal</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/An_t-Sultuine">An t-Sultuine</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/An_Daamhair">An Dàmhair</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/An_t-Samhain">An t-Samhain</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/An_Duubhlachd">An Dùbhlachd</a>),
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Mo_Reul_Iuuil">Mo Reul Iùil</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/OOran_Step">Òran Step</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Freagairt_Raonaid">Freagairt Raonaid</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/An_Co-dhuunadh">An Co-dhùnadh</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Triall_na_h-UUine">Triall na h-Ùine</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Sgeulachdan_nan_Taighean-ceeilidh">Sgeulachdan nan Taighean-céilidh</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Taigh_Tasgaidh_Chill_Donnain">Taigh Tasgaidh Chill Donnain</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/Seann_Taigh-Solais_Chalbhaigh">Seann Taigh-Solais Chalbhaigh</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/A_Ghaaidhlig">A’ Ghàidhlig</a>,
-          <a href="viewText.php?t=3_Smuaintean_fo_EEiseabhal/A_Ghaoth_an-Iar">A’ Ghaoth an-Iar tha Séideadh thar na Mara</a>
-        </div>
-        <div class="list-group-item list-group-item-action">#4: Bàrdachd Dhòmhnaill Alasdair – </div>
-        <div class="list-group-item list-group-item-action">#5: Inbhe Theàrainte dhan Ghàidhlig – </div>
-        <div class="list-group-item list-group-item-action">#6: <a href="viewText.php?t=6_Tasgadh_anns_an_UUine_Romhainn">Tasgadh anns an Ùine Romhainn</a></div>
-        <div class="list-group-item list-group-item-action">#7: Moch is Anmoch: the Gaelic poetry of Donald A. MacNeill and other Colonsay bards – 
-          <a href="viewText.php?t=7_Moch_is_Anmoch/S_Fhada_Thall_tha_Mise">’S Fhada Thall Tha Mise</a>,
-          <a href="viewText.php?t=7_Moch_is_Anmoch/Laaithean_m_OOige_le_m_Athair">Làithean m’Òige le m’Athair</a>,
-          <a href="viewText.php?t=7_Moch_is_Anmoch/Mo_Charaid_Balbh">Mo Charaid Balbh</a>,
-          <a href="viewText.php?t=7_Moch_is_Anmoch/Sruthan_Teampull_A_Ghlinne">Sruthan Teampull A’ Ghlinne</a>,
-          <a href="viewText.php?t=7_Moch_is_Anmoch/An_t-Iasgair">An t-Iasgair</a>,
-          <a href="viewText.php?t=7_Moch_is_Anmoch/An_Ceardaman">An Ceardaman</a>
-        </div>
-        <div class="list-group-item list-group-item-action">#8: Hiort: Far na Laigh a’ Ghrian – </div>
-        <div class="list-group-item list-group-item-action">#9: 
-          <a href="viewText.php?t=9_Suuil_air_Meanbh-bhiastagan">Sùil air Meanbh-bhiastagan</a>
-        </div>
-        <div class="list-group-item list-group-item-action">#10: Coimhead air an Taigh-dubh ann an Arnol</div>
-        <div class="list-group-item list-group-item-action">#1001: Saoghal Thormoid – 
-          <a href="viewText.php?t=1001_Saoghal_Thormoid/Diluain_Sinnsireachd">Diluain: Sinnsireachd</a>
-        </div>
+<?php
+$dir = '../xml/';
+$files = scandir($dir);
+foreach ($files as $nextFile) {
+  if (substr($nextFile,  strlen($nextFile)-4  ) == '.xml') {
+    $xml = new SimpleXMLElement($dir . $nextFile,0,true);
+    $ref = $xml['ref'];
+    echo '<div class="list-group-item list-group-item-action">';
+    echo '<a href="viewText.php?ref=' . $ref . '">';
+    echo $ref;
+    echo '</a>';
+    echo '</div>';
+  }
+}
+?>
       </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
--- corpas/code/viewText.php
+++ corpas/code/viewText.php
@@ -1,23 +1,32 @@
 <!doctype html>
-<html lang="en" style="height: 100%">
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="corpus.css">
-    <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"/>
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bPopup/0.11.0/jquery.bpopup.min.js"/>-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="corpus.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Corpas na Gàidhlig</title>
   </head>
-  <body style="height: 100%;">
-    <div class="container-fluid" style="height: 100%;">      
-      <div class="row" style="height: 100%;">
-        <div id="midl" class="col-6" style="overflow: auto; height: 100%;">     
+  <body>
+    <div class="container-fluid">
+
 <?php
+$text = $_GET['ref'];
+echo '<h1>' . $text . '</h1>';
+$dir = '../xml/';
+$files = scandir($dir);
+foreach ($files as $nextFile) {
+  if (substr($nextFile,  strlen($nextFile)-4  ) == '.xml') {
+    // find text element within files (recursively)
+
+
+
+  }
+}
+
+
+
+
+/*
 $text = new SimpleXMLElement("../xml/" . $_GET["t"] . ".xml", LIBXML_XINCLUDE, true);
 //$text->registerXPathNamespace('dasg', 'https://dasg.ac.uk/corpus/');
 //$subtext = $text->xpath('//dasg:text[@ref="' . $_GET["ref"] . '"]')[0];
@@ -26,11 +35,13 @@ $xsl->load('corpus.xsl');
 $proc = new XSLTProcessor;
 $proc->importStyleSheet($xsl);
 echo $proc->transformToXML($text);
-?>             
-        </div>
-        <div id="rhs" class="col-6" style="overflow: auto; height: 100%;"> <!-- the chunk info panel, on the right -->
-        </div>
-      </div>
+*/
+?>
+
+
     </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
