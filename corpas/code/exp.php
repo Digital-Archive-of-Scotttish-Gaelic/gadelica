bd5ae88f50bb837d633058670e5e8e50f193d1c4
conmaol <Mark.McConville@glasgow.ac.uk>
2020-03-18 21:43:17 +0000

    stuff

--- corpas/code/exp.php
+++ corpas/code/exp.php
@@ -1,16 +1,36 @@
 <?php
 
-$files = scandir('../xml');
+$xmls = [];
+$path = '../xml/';
+$files = scandir($path);
 foreach ($files as $nextFile) {
   if (substr($nextFile,-4)=='.xml') {
-    echo $nextFile . ' is an XML file' . PHP_EOL;
+    echo $path . $nextFile . PHP_EOL;
   }
-  else if (is_dir($nextFile)) {
-    echo $nextFile . ' is a directory' . PHP_EOL;
-  }
-  else {
-    echo $nextFile . ' is a bum' . PHP_EOL;
+  else if ($nextFile!='.' && $nextFile!='..') {
+    $path2 = $path . $nextFile . '/';
+    if (is_dir($path2)) {
+      $files2 = scandir($path2);
+      foreach ($files2 as $nextFile2) {
+        if (substr($nextFile2,-4)=='.xml') {
+          echo $path2 . $nextFile2 . PHP_EOL;
+        }
+        else if ($nextFile2!='.' && $nextFile2!='..') {
+          $path3 = $path2 . $nextFile2 . '/';
+          if (is_dir($path3)) {
+            $files3 = scandir($path3);
+            foreach ($files3 as $nextFile3) {
+              if (substr($nextFile3,-4)=='.xml') {
+                echo $path3 . $nextFile3 . PHP_EOL;
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
+
+
 
 ?>
