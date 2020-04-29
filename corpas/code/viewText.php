04598cc32f5060b8534a7a810b658c097f2dfcc2
conmaol <Mark.McConville@glasgow.ac.uk>
2020-04-29 22:26:22 +0100

    stuff

--- corpas/code/viewText.php
+++ corpas/code/viewText.php
@@ -303,8 +303,6 @@ if ($xml!='') {
   echo $proc->transformToXML($text);
 }
 ?>
-
-
     </div>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -315,8 +313,6 @@ $(function () {
   hi = $('body').attr('data-hi');
   $('#'+hi).css('background-color', 'yellow');
   $('body').animate({scrollTop: $('#'+hi).offset().top - 180},500);
-
-  
 });
     </script>
 
