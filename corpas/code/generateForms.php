418aaa32ec9d2b337581a8fb82c5ea4fa0960f27
conmaol <Mark.McConville@glasgow.ac.uk>
2020-04-30 20:32:21 +0100

    stuff

--- corpas/code/generateForms.php
+++ corpas/code/generateForms.php
@@ -11,31 +11,41 @@ foreach (new RecursiveIteratorIterator($it) as $nextFile) {
     $xml->registerXPathNamespace('dasg','https://dasg.ac.uk/corpus/');
     $status = $xml->xpath("/dasg:text/@status")[0];
     if ($status == 'tagged') {
-
       foreach ($xml->xpath("//dasg:w") as $nextWord) {
-
         $form = $nextWord;
         $lemma = (string)$nextWord['lemma'];
         if ($lemma=='') { $lemma = $form; }
+        if (strtolower($lemma[0]) == $lemma[0]) { $form = strtolower($form); }
         $pos = (string)$nextWord['pos'];
-
-        echo $form . '|' . $lemma . '|' . $pos . '<br/>';
-        /*
-        $lemma = (string)$nextWord['lemma'];
-        if ($lemma && !strpos($lemma,' ')) {
-          echo $lemma . ', ';
-          echo substr($nextFile,10) . ', ';
-          echo $nextWord['id'] . ', ';
-          echo $nextWord;
-          echo PHP_EOL;
-        }
-        */
-
+        $words[] = $form . '|' . $lemma . '|' . $pos;
       }
     }
-
   }
 }
+sort($words);
+$lexicon = array_unique($words);
+
+echo <<<HTML
+<table class="table">
+  <thead>
+    <tr><th>form</th><th>headword</th><th>pos</th></tr>
+  </thead>
+  <tbody>
+HTML;
+
+foreach ($lexicon as $nextForm) {
+  $bits = explode('|',$nextForm);
+  echo '<tr><td>';
+  echo $bits[0] . '</td><td>';
+  echo $bits[1] . '</td><td>';
+  echo $bits[2] . '</td></tr>';
+}
+
+echo <<<HTML
+  </tbody>
+</table>
+HTML;
+
 
 require_once "includes/htmlFooter.php";
 
