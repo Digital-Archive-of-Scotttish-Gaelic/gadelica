213803b8de1a83e47c5120d20fac389a676c2f9e
conmaol <Mark.McConville@glasgow.ac.uk>
2020-05-20 16:31:35 +0100

    more tags

--- corpas/code/classes/SearchView.php
+++ corpas/code/classes/SearchView.php
@@ -64,7 +64,7 @@ class SearchView
           </div>
           <div class="form-check form-check-inline">
             <input class="form-check-input" type="checkbox" id="lenitionSensitiveRadio" name="lenition" value="sensitive">
-            <label class="form-check-label" for="lenitionSensitiveRadio">lenition sensitive</label>
+            <label class="form-check-label" for="lenitionSensitiveRadio">mutation sensitive</label>
           </div>
         </div>
         <div class="form-group">
