14f3b6d834d41e5098f6db011bd6c884b4e4a050
conmaol <Mark.McConville@glasgow.ac.uk>
2020-06-23 01:00:54 +0100

    stuff

--- gramar/gd/adjectives/ainneamh.php
+++ gramar/gd/adjectives/ainneamh.php
@@ -6,39 +6,21 @@ require_once "../../includes/htmlHeader.php";
 <hr/>
 <p>Basic examples:</p>
 <ul>
-  <li><strong>Tha sin ainneamh.</strong><br/>
+  <li><strong>Tha sin <mark>ainneamh</mark>.</strong><br/>
     <small>‘That’s fine/excellent.’ [#2 ]</small></li>
-  <li><strong>Agus ’s ann glè ainneamh a bhiodh sin.</strong><br/>
+  <li><strong>Agus ’s ann <mark>glè ainneamh</mark> a bhiodh sin.</strong><br/>
     <small>‘And that would be very rare.’ [CM 20]</small></li>
 </ul>
-<p>mmm</p>
+<p>Some idiomatic uses:</p>
 <ul>
-  <li><strong>Is ainneamh sionnach a tha air fhàgail.</strong><br/>
-    <small>‘There are few foxes left.’ [CM 20]</small></li>
-  <li><strong>Is ainneamh am fear anns a’ bhaile seo nach do ghabh bradan aig àm air choreigin</strong><br/>
-    <small>‘There is scarcely a man in this township who has not taken a salmon at some time or other.’ [CM 20]</small></li>
-</ul>
-
-
-
-
-<p>mmm</p>
-<ul>
-  <li><strong>’S ann ainneamh dha-rìribh a tha rud cho oillteil a’ tighinn air pòsadh ris an rud a thàinig air pòsadh Ailig.</strong><br/>
-    <small>‘Something as horrible seldom affects a marriage as the thing which affected Alec’s marriage.’ [#2 103]</small></li>
-  <li><strong>’S ann ainneamh a tha i san aon taigh rium.</strong><br/>
-    <small>‘She is seldom in the same room as me.’ [#2 105]</small></li>
-  <li><strong>’S ann glè ainneamh a chitheadh duine e air a sgeadachadh mar duine-uasal.</strong><br/>
-    <small>‘He was very seldom seen dressed like a gentleman.’ [#2 103]</small></li>
-  <li><strong>Is ainneamh a thàinig e.</strong><br/>
-    <small>‘He seldom came.’ [CM 20]</small></li>
+  <li><strong><a href="../copular_idioms/is_ainneamh.php">is ainneamh ...</a></strong></li>
+  <li><strong><a href="../copular_idioms/is_ann_ainneamh_a.php">is (ann) ainneamh a/nach ...</a></strong></li>
 </ul>
 <p>CM lists the derived feminine noun <strong>ainneamhag</strong> as meaning ‘phoenix’</p>
 <ul>
   <li><strong>mar <mark>ainneamhag</mark> ag èirigh bhon luaithre</strong><br/>
     <small>‘like a phoenix rising from the ashes’ [#2 ]</small></li>
 </ul>
-
 
 <?php
 require_once "../../includes/htmlFooter.php";
