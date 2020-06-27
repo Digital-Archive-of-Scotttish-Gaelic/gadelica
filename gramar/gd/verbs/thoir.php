15584117cf3a304a59e844da539a4e91e6ce61ce
conmaol <Mark.McConville@glasgow.ac.uk>
2020-06-27 15:00:54 +0100

    stuff

--- gramar/gd/verbs/thoir.php
+++ gramar/gd/verbs/thoir.php
@@ -26,11 +26,16 @@ require_once "../../includes/htmlHeader.php";
   <li>Bheireadh e a’ mhòr-chuid dhen iasg dha na bochdan [112]</li>
   <li>Thug mi dha an litir  [116]</li>
   <li>Rinn e sgrìobhadh air pìos pàipeir agus thug e do Phòl e. [9-10]</li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
+  <li>Feumaidh tu a h-uile gòraich a leigeil seachad agus a h-uile litir a thig bho na pools a thoirt dhòmhsa gus an loisg mi iad.” [10]</li>
+  <li>“Thug iad dhomh an seòladh ceàrr anns a’ bhùth. [14]</li>
+  <li>Chuimhnich mi an uair sin air a’ chofaidh a thug mi dhaibh, [15]</li>
+  <li>Tha Seanailear Màsasmo a-nis a’ riaghladh Dyger agus a’ dol a thoirt dhuinn cùmhnant luachmhor airson armachd. [19]</li>
+  <li>canaidh sinn gu bheil leth an taighe mar oifis, agus bheir iad dhut pàirt dhen chosgais. [27]</li>
+  <li>thug e pìos pàipeir dhan t-Seilcheig. [30]</li>
+  <li> Thug i dhomh e [37]</li>
+  <li>Thug e dha còig notaichean airson innse dè chunnaic e sa chiste. [37]</li>
+  <li>Bha i air cluinntinn nach robh sinn air toirt dha na goireasan bu chòir a bhith aig duine dall. [40]</li>
+  <li>Mhothaich mi gu robh sùilean Màiri a’ sileadh agus thug mi dhi mo neapraig-pòcaid. [43]</li>
 </ul>
 
 <p>take/bring (to)</p>
@@ -62,10 +67,9 @@ require_once "../../includes/htmlHeader.php";
   <li>Bha bliadhna bho thòisich e a’ falbh leatha, a’ bhliadhna a fhuair e àrdachadh gu inbhe lioftanant san Nèibhi agus a thug e Magaidh dà uair a Lunnainn. [124]</li>
   <li>Thug i a-steach e airson drama [126]</li>
   <li>Nam fuiricheadh e gus an tigeadh athair, dh’fhaodadh e iasg a thoirt thuice cuideachd. [123]</li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
+  <li> Chaidh a thoirt a Ghlaschu dhan ospadal. [18]</li>
+  <li>Rinn mise ullachadh airson an dust a thoirt dhachaigh. [19]</li>
+  <li>theirig gu tuathanas an Rosaich agus thoir loda todhair chun a’ ghàrraidh agam.” [25]</li>
 </ul>
 
 <p>take from/out of</p>
@@ -81,15 +85,15 @@ require_once "../../includes/htmlHeader.php";
   <li>“ ’S e copaidh a tha seo,” ars esan, a’ toirt a mach a’ phàipeir a chuir mi chun phoilis. [116]</li>
   <li>Thug e uaithe am poca [123]</li>
   <li>Thug Eilidh a-mach botal agus ghabh iad drama. [10]</li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
+  <li>Cha leig mi leas na soithichean spaideil a thoirt a-mach.” [17]</li>
+  <li>Leugh mi mu dheidhinn bean-phòsta a thàinig dhachaigh à ospadal leis an leanabh cheàrr, agus mu lannsair a thug sùil às an duine cheàrr. [19] remove</li>
+  <li>’S e dalta a bh’ ann am Frank, a thug Mòr a’ Mheug à Glaschu, [21]</li>
+  <li>Nach toir iad bhuam an taigh?” [28]</li>
+  <li>“Tha C mar chearcall le glaim air a thoirt às a mhionach, [34]</li>
+  <li>“Thug e an iuchair à pòcaid Theàrlaich [37]</li>
+  <li>Dh’fhosgail i preas aodaich agus thug i a-mach poca plastaig làn de bhuinn airgid, [43]</li>
+  <li>Glè thric ’s e eathraichean beaga a bhiodh gam thoirt bho eilean gu eilean [44]</li>
+  <li>Aig an àm bha mo dhreuchd gam thoirt air feadh nan Eilean Siar bho Leòdhas gu Bhatarsaigh, agus bhon Eilean Sgitheanach gu Eilean nam Muc. [44]</li>
 </ul>
 
 <p>take/bring to do sth</p>
@@ -101,25 +105,7 @@ require_once "../../includes/htmlHeader.php";
 <p>take/bring (with)</p>
 <ul>
   <li>“Bheir sinn leinn an dubhan,” [56]</li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
+  <li>“Thoir leat gràpa agus theirig gu tuathanas an Rosaich [25]</li>
 </ul>
 
 <p>idioms</p>
@@ -158,22 +144,29 @@ require_once "../../includes/htmlHeader.php";
   <li> Glè thric, ma bhios Gaidheil a-staigh, canaidh i facail Ghàidhlig mum dheidhinn a bheir gàire orra. [104]</li>
   <li>ged as ise a thug bòid a bhith umhail dhàsan, ’s e esan a bha umhail dhise [115]</li>
   <li>Ged a chitheadh duine am parsail anns a’ char, cha tugadh e smaoin eile air. [120]</li>
+  <li>Ghlas mi an doras às mo dhèidh agus cha tug mi smaoin tuilleadh air a’ ghnothaich. [14]</li>
   <li>Thug an criothnachadh a fhuair e gu chiall e. [125]</li>
   <li>Bha e na rùn croga bàrr a thoirt thuice. [123]</li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
+  <li>Ach nuair a chuala iad mo sgeulachd, mhothaich mi gun tug iad sùil air a chèile. [15]</li>
+  <li>Nuair a bha iad a’ falbh, dh’fhaighnich fear aca dhomh na mhothaich mi an robh miotagan air na daoine, agus thuirt mi gu robh. “Cha tug iad dhiubh idir iad?” ars esan. [15]</li>
+  <li>gun teagamh thug iad dhiubh na miotagan fhad ’s a bha iad ga òl. [15]</li>
+  <li>Chuala mi e ag ràdh uair is uair rud a thug gàire orm. [17]</li>
+  <li>Chuala mi dotair ag ràdh nach robh e a’ toirt do charaid dhomh ach sia mìosan eile [18-19]</li>
+  <li>chuir e a-mach iad gun freagairt a thoirt dhaibh. [21]</li>
+  <li>Faodaidh mise an taigh a rannsachadh agus a h-uile càil a tha na bhroinn a thoirt às a chèile.” [22]</li>
+  <li>Fhuair na daoine sin obair dha a’ glanadh uinneagan, rud a thug gàireachdainn gu leòr oirnne. [23]</li>
+  <li>Bheir iad gu lagh thu. [26]</li>
+  <li>cha toir mi gu lagh thu ma nì thu car obrach dhòmhsa.” [29]</li>
+  <li>cha tug sinne cluais dhan chasaid sin. [31]</li>
+  <li>Thug mi dha dathadh is dà thionndadh. [38]</li>
+  <li>’S e thug gàire orm na ‘pieces of eight’. [39]</li>
+  <li>ged a sheall e dhomh e, cha tug e guth air a’ chiste-mhara [36]</li>
+  <li>’S e gu robh a fhradharc a’ fàs lag a thug air fuireach air tìr. [36]</li>
+  <li>“Tha e iongantach gun tug thu iomradh air bàthadh,” [46]</li>
+  <li>“Cha do rinn thu ach mar a dhèanadh duine sam bith anns an t-suidheachadh sin,” arsa mise, airson cofhurtachd a thoirt dha. [47]</li>
+  <li> thug mi iomradh air mo chompanach [47]</li>
+  <li>gun guth a thoirt air an rud a bha a’ cur dragh air a chogais [47]</li>
+  <li>thug e sùil glè neònach orm. [47]</li>
 </ul>
 
 
