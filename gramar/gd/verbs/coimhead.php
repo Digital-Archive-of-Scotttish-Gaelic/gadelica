f903ba993048dfae7d540e923f5fb3c9231f5e62
conmaol <Mark.McConville@glasgow.ac.uk>
2020-07-07 13:53:22 +0100

    stuff

---
+++ gramar/gd/verbs/coimhead.php
@@ -0,0 +1,96 @@
+<?php
+require_once "../../includes/htmlHeader.php";
+?>
+<h1>coimhead</h1>
+
+coimhead
+
+INTRANS
+Mar a tha thu a’ coimhead, ghlac an teine mi, ach fhuair mi air falbh, agus thog eathar mi.
+
+ADJ
+Cha do smaoinich mi aon uair gur e eucorach a bha anns a’ ghille a bha coimhead cho ceart, [50]
+Saoilidh mise gur e balach liugach a th’ ann.” Rinn Syd dealbh air na inntinn, ach cha b’ ann liugach a bha esan ga choimhead idir. [60]
+Bha e duilich a chreidsinn gun rinn duine a bha coimhead cho stòlda an rud uabhasach a rinn e.  [61]
+’S e Murd a’ Mhinisteir am prìomh pheata, balach eireachdail nach robh cho beag-lochd ’s a bha e a’ coimhead. [69-70]
+bha an t-àite a’ coimhead cho marbh. [117]
+“Tha ’n t-àite seo coimhead glè fhallain,” [17]
+
+TRANS
+‘see, observe someone/something‘
+Gu mì-fhortanach dhaibhsan, chaidh an coimhead agus an aithneachadh. [52]
+Chuir e cuireadh thuige, ann an litir chun a’ phàipeir-naidheachd, a thighinn a choimhead nan rudan a bha esan agus a bhean a’ falach. [66]
+“Tha thu airson coimhead àite nan Caimbeulach.” [67]
+Nuair a bhiodh sinn a’ dol dhan sgoil agus a’ tighinn dhachaigh, stadadh sinn a’ coimhead nam flùraichean àlainn, geal, dearg is buidhe. [69]
+aig àm teatha na maidne chaidh Ruairidh ga choimhead gun bhonaid idir. [91]
+Stad e a’ coimhead an duine airson greis.  [91]
+Ach bha e air a coimhead còmhla ri fear a’ chàir roimhe. [124]
+Bha mi a’ dol ga choimhead a h-uile madainn ’s a h-uile feasgar,  [18]
+Cha robh i ga choimhead ach uair no dhà sa bhliadhna. [43]
+Bha e air an duine seo a choimhead roimhe. [118]
+
+TRANS + AG
+Anns a’ chùirt thuirt e gu robh e gan coimhead a’ gàireachdainn, a’ fanaid air. [75]
+
+
+
+AIR
+‘(go/come and) see, visit somebody‘
+Mura dèan thu sin, chan e mise a thig a choimhead ort idir.” [55]
+Ach nuair a chaidh an dotair a choimhead orra, cha b’ ann air an slàinte a bha aire ach air am faighinn a-mach às an taigh. [65]
+Seachdain mus deach e a choimhead orra, rinn e toll tro bhalla an taighe eadar e fhèin agus iadsan, [65]
+’S e an Daolag an aon fhear a tha beò fhathast, ach nuair a chaidh mi dhan ospadal a choimhead air cha robh cuimhne aige orm fhìn no air càil a tha anns an eachdraidh sa. [72]
+An ceann seachdaine thàinig lorg-phoileas a choimhead orm. [116]
+ am feasgar seo bha e mì-fhoighidneach, oir bha e a’ dol a choimhead air a leannan, Magaidh, sa bhaile mhòr. [123]
+thàinig Sir Claude Auchinlech dhan ospadal a choimhead air. [16]
+Cha tàinig i choimhead air a’ Chaiptean ach uair no dhà a-riamh.  [40]
+
+an dèidh dha a bhith a’ coimhead air a bhean san ospadal [117]
+
+‘regard something as’
+Bha sinne a’ coimhead air mar aon dhe na prìomh bheusan! [112] poaching
+
+‘inspect, look at something’
+Chaidh iad an sin a choimhead air na craobhan, [30]
+‘Chan eil cead againn a dhol a choimhead air a’ chiste [38]
+
+
+
+RI
+‘look at something/someone’
+Chuir na leugh Syd na thuaineal e. Bha inntinn bàn a’ coimhead ris an litir. [59]
+Nuair a chunnaic mi a dhealbh air an teilidh, ’s ann ri aghaidh an duine a mharbh mi a bha mi coimhead. [63]
+’S ann riumsa agus ri Doilean a bha e a’ coimhead. [71]
+Thog e catalog agus thòisich e a’ leigeil air gur ann a’ coimhead ris a bha sinn. [32]
+
+
+GUN/NACH
+Tha mi coimhead gu bheil tinneas craicinn ort. [67]
+ “Tha mi coimhead gu bheil thu a’ dèanamh nam pools,” [10]
+ “Tha mi a’ coimhead gu bheil mi aig an taigh cheàrr. [13]
+Ach bha feadhainn a’ coimhead nach freagradh poileasaidh an t-siorraidh a h-uile eucoir. [74]
+
+QUESTION
+Bha e ag ràdh gur ann airson coimhead càit an laigheadh na geòidh a bha e. [79]
+
+AIRSON
+Bha am poileas a’ coimhead airson Fiat bheag dhearg, [59]
+
+
+AN DÈIDH
+chuir an t-ospadal Sandaidh còmhla ris airson coimhead às a dhèidh. [58]
+Bha bràithrean Ruairidh a’ dol a choimhead às dèidh an taighe agus an sprèidh. [90]
+dh’iarr iad ormsa a dhol a Lunnainn a choimhead às dèidh ghnothaichean. [8]
+mar as àbhaist bha mi a’ coimhead às dèidh an àite  [13]
+
+IDIOM?
+dh’fhàs an là cho garbh ’s gun deach Dan ga coimhead dhachaigh sàbhailt. [126]
+
+
+
+
+
+
+<?php
+require_once "../../includes/htmlFooter.php";
+?>
