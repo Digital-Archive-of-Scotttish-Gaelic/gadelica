a1238cdec2f731fb3126c9ac537a584c770b5188
steviebarrett <mail@steviebarrett.com>
2020-08-28 05:40:58 +0100

    Possible fix for accents not displaying in Entry Forms view

--- corpas/code/classes/Entries.php
+++ corpas/code/classes/Entries.php
@@ -26,8 +26,7 @@ SQL;
 			$sth = $dbh->prepare($sql);
 			$sth->execute(array(":lemma"=>$entry->getLemma(), ":wordclass"=>$entry->getWordclass()));
 			while ($row = $sth->fetch()) {
-				//$wordform = mb_strtolower($row["wordform"]);  //make all forms lowercase
-				$wordform = $row["wordform"];
+				$wordform = mb_strtolower($row["wordform"], "UTF-8");  //make all forms lowercase
 				$slipId = $row["auto_id"];
 				$entry->addForm($wordform, $slipId);
 				$slipMorphResults = Slips::getSlipMorphBySlipId($slipId);
