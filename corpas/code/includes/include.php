719a37291da25cfc8a9322bcdfd53ac709bd9856
steviebarrett <mail@steviebarrett.com>
2020-09-24 09:20:37 +0100

    Bug fix with printSlips array

--- corpas/code/includes/include.php
+++ corpas/code/includes/include.php
@@ -3,7 +3,9 @@
 session_start();
 
 //TODO: consider relocating this SB
-$_SESSION["printSlips"] = array();
+if (!$_SESSION["printSlips"]) {
+  $_SESSION["printSlips"] = array();
+}
 
 //ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 
