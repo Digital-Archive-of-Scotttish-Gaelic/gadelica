b523da60c27c0706d18ed5efb0f08ba4e495bdb4
conmaol <Mark.McConville@glasgow.ac.uk>
2020-10-27 23:20:15 +0000

    small edit

--- corpas/code/index2.php
+++ corpas/code/index2.php
@@ -5,36 +5,22 @@ require_once "includes/htmlHeader.php";
 
 //$action = isset($_GET["a"]) ? $_GET["a"] : "";
 $module = isset($_GET["m"]) ? $_GET["m"] : "";
-$controller = null;
+//$controller = null;
 
 switch ($module) {
 	case "corpus":
-		$controller = new corpus2();
+		new corpus2();
 		break;
 
 	/*
-	case "text":
-		$textId = $_GET["textId"];
-		$controller = new text_sql($textId);
+	// TO COME
+	case "collection":
+		$controller = new collection();
 		break;
-	case "writer":
-		$controller = new writer_sql();
+	case "dictionary":
+		$controller = new dictionary();
 		break;
-	case "search":
-		$origin = "index.php?m=search";
-		$controller = new corpussearch($origin);
-		break;
-	case "slips":
-		$controller = new slipbrowse();
-		break;
-	case "slip":
-		$slipId = !empty($_GET["auto_id"]) ? $_GET["auto_id"] : false;
-		$controller = new slip($slipId);
-		break;
-	case "entries":
-		$controller = new entries();
-		break;
-	case "docs":
+	case "documentation":
 		$controller = new documentation();
 		break;
 	*/
