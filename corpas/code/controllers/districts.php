fb719d175c48355008c2aaa6b081d2149ab32419
conmaol <Mark.McConville@glasgow.ac.uk>
2020-11-11 00:15:38 +0000

    more tags

--- corpas/code/controllers/districts.php
+++ corpas/code/controllers/districts.php
@@ -17,34 +17,12 @@ class districts
 					$model = new models\districts();
 					$view = new views\districts($model);
 					$view->show();
-				} else { // view particular writer
+				} else { // view particular district
 					$model = new models\district($id);
 					$view = new views\district($model);
 					$view->show();
 				}
 				break;
-//			case "edit":
-//				if ($id == "0") { // add new writer
-//					$model = new models\writer(null); //create a dummy writer object
-//				} else { // edit existing writer
-//					$model = new models\writer($id);
-//				}
-//				$view = new views\writer($model);
-//				$view->show("edit");
-//				break;
-//			case "save":
-//				models\writers::save($_POST);
-//				if (!$id) {
-//					$model = new models\writers();
-//					$view = new views\writers($model);
-//					$view->show();
-//				}
-//				else {
-//					$model = new models\writer($id);
-//					$view = new views\writer($model);
-//					$view->show("browse");
-//				}
-//				break;
 		}
 	}
 }
