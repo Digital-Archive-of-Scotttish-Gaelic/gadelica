8ac89ae853f2143ed82a5e01fe006f7dc9e3883d
conmaol <Mark.McConville@glasgow.ac.uk>
2020-11-18 00:24:24 +0000

    grammar work

---
+++ gramar/gd/prepositions/mar.php
@@ -0,0 +1,168 @@
+<?php
+require_once "../../includes/htmlHeader.php";
+?>
+<h1>mar</h1>
+<hr/>
+<h3>1. <em>mar</em> + NP</h3>
+<p>The simplest use of <strong>mar</strong> is as a preposition-like word governing a noun in the nominative case, 
+and typically corresponding to the English preposition ‘like’.</p>
+<h5>1.1. <em>bi mar</em></h5>
+<p>The most basic use of the preposition <strong>mar</strong> is as predicate of the copula verb <strong>bi</strong>, i.e. ‘be like’.
+In this construction, the entity denoted by the subject of the verb is claimed to be similar in some significant way to the entity 
+denoted by the noun governed by <strong>mar</strong>.</p>
+<ul>
+  <li class="ex" data-slip="2787"></li>
+  <li class="ex" data-slip="2789"></li>
+  <li class="ex" data-slip="2793"></li>
+  <li class="ex" data-slip="2825"></li>
+  <li class="ex" data-slip="2828"></li>
+  <li class="ex" data-slip="2829"></li>
+  <li class="ex" data-slip="2842"></li>
+</ul>
+<p>similes?</p>
+<p>Sometimes the copula verb is implicit –</p>
+<ul>
+  <li class="ex" data-slip="2833"></li>
+</ul>
+<p>Sometimes, the preposition is intensified by the adverb <strong>dìreach</strong> ‘just like’ –</p>
+<ul>
+  <li class="ex" data-slip="2785"></li>
+  <li class="ex" data-slip="2808"></li>
+</ul>
+<h5>1.2. Adnominal <em>mar</em> + NP</h5>
+<p>When <strong>mar</strong> is used to modify another noun, it usually assumes something like an implicit copula, 
+as in the entity denoted by the modified noun is claimed to be similar to the entity denoted by the noun governed by <strong>mar</strong> –</p>
+<ul>
+  <li class="ex" data-slip="2788"></li>
+  <li class="ex" data-slip="2792"></li>
+  <li class="ex" data-slip="2846"></li>
+</ul>
+<p>The following example is different in that the two nouns denote the <em>same</em> entity –</p>
+<ul>
+  <li class="ex" data-slip="2805"></li>
+</ul>
+<h5>1.3. Adverbal <em>mar</em> + NP</h5>
+<p>The entity denoted by one of the dependent NPs of the modified verb is claimed to be similar to the entity denoted by the noun governed by <strong>mar</strong>.</p>
+<p>Subject-controlled –</p>
+<ul>
+  <li class="ex" data-slip="2783"></li>
+  <li class="ex" data-slip="2786"></li>
+  <li class="ex" data-slip="2794"></li>
+  <li class="ex" data-slip="2806"></li>
+</ul>
+<p>Object-controlled –</p>
+<ul>
+  <li class="ex" data-slip="2790"></li>
+  <li class="ex" data-slip="2827"></li>
+</ul>
+<p>Other –</p>
+<ul>
+  <li class="ex" data-slip="2807"></li>
+  <li class="ex" data-slip="2791"></li>
+</ul>
+<h5>1.4. Sentential <em>mar sin</em></h5>
+<p>so, therefore, for that reason</p>
+<ul>
+  <li class="ex" data-slip="2768"></li>
+  <li class="ex" data-slip="2769"></li>
+  <li class="ex" data-slip="2770"></li>
+  <li class="ex" data-slip="2771"></li>
+  <li class="ex" data-slip="2772"></li>
+  <li class="ex" data-slip="2773"></li>
+  <li class="ex" data-slip="2775"></li>
+  <li class="ex" data-slip="1776"></li>
+</ul>
+<p>is ann mar sin a ... ‘it was thus that ...’</p>
+<ul>
+  <li class="ex" data-slip="2774"></li>
+</ul>
+<h5>1.5. Idiomatic uses</h5>
+<ul>
+  <li><strong>ag obair mar</strong> ‘working as’
+    <ul>
+      <li class="ex" data-slip="2801"></li>
+      <li class="ex" data-slip="2837"></li>
+    </ul>
+  </li>
+  <li><strong>suidhich mar</strong> ‘appoint as’
+    <ul>
+      <li class="ex" data-slip="2835"></li>
+    </ul>
+  </li>
+  <li><strong>dreuchd mar</strong> ‘a job/career as’
+    <ul>
+      <li class="ex" data-slip="2803"></li>
+    </ul>
+  </li>
+  <li><strong>dèan beòshlaint mar</strong> ‘make a living as’
+    <ul>
+      <li class="ex" data-slip="2782"></li>
+    </ul>
+  </li>
+  <li><strong>meas mar</strong> ‘regard something as’
+    <ul>
+      <li class="ex" data-slip="2730"></li>
+      <li class="ex" data-slip="2784"></li>
+    </ul>
+  </li>
+  <li><strong>coimhead air ... mar</strong> ‘see something as’
+    <ul>
+      <li class="ex" data-slip="2831"></li>
+    </ul>
+  </li>
+  <li><strong>smaoinich air ... mar</strong> ‘think of something as’
+    <ul>
+      <li class="ex" data-slip="2834"></li>
+    </ul>
+  </li>
+  <li><strong>ainmeil mar</strong> ‘famous as’
+    <ul>
+      <li class="ex" data-slip="2802"></li>
+    </ul>
+  </li>
+  <li><strong>iomraiteach mar</strong> ‘renowned as’
+    <ul>
+      <li class="ex" data-slip="2809"></li>
+    </ul>
+  </li>
+  <li><strong>air a sgeadachadh mar</strong> ‘dressed as’
+    <ul>
+      <li class="ex" data-slip="2804"></li>
+      <li class="ex" data-slip="2810"></li>
+      <li class="ex" data-slip="2824"></li>
+    </ul>
+  </li>
+  <li><strong>sgrìobh mar</strong> ‘write as’
+    <ul>
+      <li class="ex" data-slip="2832"></li>
+    </ul>
+  </li>
+  <li><strong>tachartasan mar</strong> ‘one’s exploits as’
+    <ul>
+      <li class="ex" data-slip="2826"></li>
+    </ul>
+  </li>
+</ul>
+<hr/>
+<h3>2. <em>mar a</em></h3>
+
+
+<h5>2.5. mar a is àbhaist</h5>
+<p>as usual</p>
+<ul>
+  <li class="ex" data-slip="2845"></li>
+</ul>
+
+<hr/>
+<h3>3. <em>mar gun</em></h3>
+<p>hypothetical – as if</p>
+<ul>
+  <li class="ex" data-slip="2838"></li>
+  <li class="ex" data-slip="2839"></li>
+  <li class="ex" data-slip="2840"></li>
+  <li class="ex" data-slip="2841"></li>
+</ul>
+
+<?php
+require_once "../../includes/htmlFooter.php";
+?>
