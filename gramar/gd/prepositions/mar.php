45eab056a3a00788d4dfc51dac2efd22e4088d50
conmaol <Mark.McConville@glasgow.ac.uk>
2020-11-21 13:20:21 +0000

    more examples

--- gramar/gd/prepositions/mar.php
+++ gramar/gd/prepositions/mar.php
@@ -35,7 +35,7 @@ For example:</p>
   <li class="ex" data-slip="2846"></li>
 </ul>
 <p>The following adnominal example is deceptive in that it is asserted that the two nouns denote the <em>same</em> entity, 
-rather than two distinct but similar entities:</p>
+rather than two distinct but similar entities. It may be an example of English influence:</p>
 <ul>
   <li class="ex" data-slip="2805"></li>
 </ul>
@@ -57,7 +57,7 @@ The verb within the relative clause denotes an event which is assumed to have ac
   <li class="ex" data-slip="2880"></li>
 </ul>
 <p>In its second manner adverbal use, <strong>mar</strong> governs a (positive) complement clause, introduced by the dependent verb particle <strong>gun</strong>. 
-The subordinate verb is usually a conditional form of bi (or a past tense form with conditional force). 
+The subordinate verb is usually a conditional form of <strong>bi</strong> (or a past tense form with conditional force). 
 Here the assumption is that the state denoted by the subordinate clause is purely hypothetical. For example:</p>
 <ul>
   <li class="ex" data-slip="2841"></li>
@@ -68,7 +68,7 @@ Here the assumption is that the state denoted by the subordinate clause is purel
   <li class="ex" data-slip="2838"></li>
   <li class="ex" data-slip="2839"></li>
 </ul>
-<p>Prepositional mar can also be used to introduce as hypothetical manner adverb, for example:</p>
+<p>Prepositional <strong>mar</strong> can also be used to introduce a hypothetical manner adverb, for example:</p>
 <ul>
   <li class="ex" data-slip="2794"></li>
   <li class="ex" data-slip="2790"></li>
@@ -78,49 +78,44 @@ Here the assumption is that the state denoted by the subordinate clause is purel
 <h3>3. Other adverbal uses of <em>mar</em></h3>
 <p><a data-toggle="collapse" href="#other-adverbal">show/hide</a></p>
 <div class="collapse" id="other-adverbal">
+<p>Both as a preposition and a conjunction, <strong>mar</strong> is used in a range of other, more idiomatic adverbal expressions.</p>
+<h5><em>mar sin</em></h5>
+<p>‘so’, ‘therefore’, ‘for that reason’</p>
+<ul>
+  <li class="ex" data-slip="2768"></li>
+  <li class="ex" data-slip="2771"></li>
+  <li class="ex" data-slip="2774"></li>
+</ul>
 <h5>mar as àbhaist</h5>
+<p>‘as usual’</p>
 <ul>
   <li class="ex" data-slip="2907"></li>
   <li class="ex" data-slip="2910"></li>
 </ul>
-
-
+<p>cf. <strong>àbhaist</strong></p>
 <h5>mar as trice</h5>
+<p>‘usually’</p>
 <ul>
   <li class="ex" data-slip="2884"></li>
   <li class="ex" data-slip="2914"></li>
-  <li class="ex" data-slip="2916"></li>
-  <li class="ex" data-slip="2915"></li>
 </ul>
-
-<h5><em>mar sin</em></h5>
-<p>so, therefore, for that reason</p>
-<ul>
-  <li class="ex" data-slip="2768"></li>
-  <li class="ex" data-slip="2769"></li>
-  <li class="ex" data-slip="2770"></li>
-  <li class="ex" data-slip="2771"></li>
-  <li class="ex" data-slip="2772"></li>
-  <li class="ex" data-slip="2773"></li>
-  <li class="ex" data-slip="2775"></li>
-  <li class="ex" data-slip="1776"></li>
-</ul>
-<p>is ann mar sin a ... ‘it was thus that ...’</p>
-<ul>
-  <li class="ex" data-slip="2774"></li>
-</ul>
-
-<h5>mar a thuirt</h5>
-<p>as someone said</p>
+<p>cf. <strong>tric</strong></p>
+<h5>mar a thuirt i</h5>
+<p>‘as she said’</p>
 <ul>
   <li class="ex" data-slip="2895"></li>
   <li class="ex" data-slip="2905"></li>
-  <li class="ex" data-slip="2904"></li>
   <li class="ex" data-slip="2897"></li>
 </ul>
-<p>thuirt/ràdh but canas/canadh</p>
+<p>Note that future and conditional forms of the irregular verb <strong>abair</strong> are usually avoided in many modern dialects, 
+in favour of either forms of the regular verb <strong>can</strong> or progressive constructions with the verbal noun <strong>ràdh</strong>.</p>
+<h5>Others</h5>
+<ul>
+  <li class="ex" data-slip="2783"></li>
+  <li class="ex" data-slip="2780"></li>
+  <li class="ex" data-slip="2781"></li>
+</ul>
 </div>
-
 
 <hr/>
 
