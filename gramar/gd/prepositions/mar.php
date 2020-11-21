9e5c292250e2ae89890d67f544e359d5ea27ad89
conmaol <Mark.McConville@glasgow.ac.uk>
2020-11-21 00:55:39 +0000

    more examples

--- gramar/gd/prepositions/mar.php
+++ gramar/gd/prepositions/mar.php
@@ -4,6 +4,8 @@ require_once "../../includes/htmlHeader.php";
 <h1>mar</h1>
 <hr/>
 <h3>1. Predicative uses of <em>mar</em></h3>
+<p><a data-toggle="collapse" href="#predicative">show/hide</a></p>
+<div class="collapse" id="predicative">
 <p>A common use of <strong>mar</strong> is as a preposition-like word governing a noun in the nominative case, 
 and corresponding to the English preposition ‘like’. In this sense, <strong>mar</strong> often appears as the predicate of the copular verb <strong>bi</strong>, 
 i.e. ‘be like’ – asserting that the entity denoted by the subject of <strong>bi</strong> is <em>similar</em> in some significant way to the entity 
@@ -38,8 +40,11 @@ rather than two distinct but similar entities:</p>
   <li class="ex" data-slip="2805"></li>
 </ul>
 <p>cf. <strong>coltach ri</strong></p>
-<hr/>
+</div>
+
 <h3>2. Manner adverbal uses of <em>mar</em></h3>
+<p><a data-toggle="collapse" href="#manner">show/hide</a></p>
+<div class="collapse" id="manner">
 <p>Another common use of <strong>mar</strong> is as a kind of subordinating conjunction expressing a concept similar to a manner adverb, 
 i.e. modifying a verb and describing in more detail <em>how</em> the action denoted by that verb was carried out. 
 When used as a conjunction, <strong>mar</strong> itself governs a verb rather than a noun, and asserts that the action denoted by 
@@ -63,40 +68,65 @@ Here the assumption is that the state denoted by the subordinate clause is purel
   <li class="ex" data-slip="2838"></li>
   <li class="ex" data-slip="2839"></li>
 </ul>
-<p>closely related i.e. as if they were ...</p>
+<p>Prepositional mar can also be used to introduce as hypothetical manner adverb, for example:</p>
 <ul>
-  <li class="ex" data-slip="2786"></li>
   <li class="ex" data-slip="2794"></li>
-  <li class="ex" data-slip="2806"></li>
   <li class="ex" data-slip="2790"></li>
 </ul>
+<p>The manner phrases in these examples can be paraphrased as ‘as if he were a lawyer’ and ‘as if we were a squad of soldiers’.</p>
+</div>
+<h3>3. Other adverbal uses of <em>mar</em></h3>
+<p><a data-toggle="collapse" href="#other-adverbal">show/hide</a></p>
+<div class="collapse" id="other-adverbal">
+<h5>mar as àbhaist</h5>
+<ul>
+  <li class="ex" data-slip="2907"></li>
+  <li class="ex" data-slip="2910"></li>
+</ul>
+
+
+<h5>mar as trice</h5>
+<ul>
+  <li class="ex" data-slip="2884"></li>
+  <li class="ex" data-slip="2914"></li>
+  <li class="ex" data-slip="2916"></li>
+  <li class="ex" data-slip="2915"></li>
+</ul>
+
+<h5><em>mar sin</em></h5>
+<p>so, therefore, for that reason</p>
+<ul>
+  <li class="ex" data-slip="2768"></li>
+  <li class="ex" data-slip="2769"></li>
+  <li class="ex" data-slip="2770"></li>
+  <li class="ex" data-slip="2771"></li>
+  <li class="ex" data-slip="2772"></li>
+  <li class="ex" data-slip="2773"></li>
+  <li class="ex" data-slip="2775"></li>
+  <li class="ex" data-slip="1776"></li>
+</ul>
+<p>is ann mar sin a ... ‘it was thus that ...’</p>
+<ul>
+  <li class="ex" data-slip="2774"></li>
+</ul>
+
+<h5>mar a thuirt</h5>
+<p>as someone said</p>
+<ul>
+  <li class="ex" data-slip="2895"></li>
+  <li class="ex" data-slip="2905"></li>
+  <li class="ex" data-slip="2904"></li>
+  <li class="ex" data-slip="2897"></li>
+</ul>
+<p>thuirt/ràdh but canas/canadh</p>
+</div>
 
 
 <hr/>
 
 
-
-<h5>1.3. Adverbal <em>mar</em> + NP</h5>
-<p>The entity denoted by one of the dependent NPs of the modified verb is claimed to be similar to the entity denoted by the noun governed by <strong>mar</strong>.</p>
-<p>Subject-controlled –</p>
-<ul>
-  <li class="ex" data-slip="2783"></li>
-  <li class="ex" data-slip="2786"></li>
-  <li class="ex" data-slip="2794"></li>
-  <li class="ex" data-slip="2806"></li>
-</ul>
-<p>Object-controlled –</p>
-<ul>
-  <li class="ex" data-slip="2790"></li>
-  <li class="ex" data-slip="2827"></li>
-</ul>
-<p>Other –</p>
-<ul>
-  <li class="ex" data-slip="2807"></li>
-  <li class="ex" data-slip="2791"></li>
-</ul>
-
-<h5>1.5. Idiomatic uses</h5>
+<!--
+<h5>Idiomatic uses</h5>
 <ul>
   <li><strong>ag obair mar</strong> ‘working as’
     <ul>
@@ -163,44 +193,11 @@ Here the assumption is that the state denoted by the subordinate clause is purel
     </ul>
   </li>
 </ul>
-<hr/>
-<h3>Sentential adverbs</h3>
-<h5>mar a is àbhaist</h5>
-<p>as usual</p>
-<ul>
-  <li class="ex" data-slip="2845"></li>
-  <li class="ex" data-slip="2907"></li>
-</ul>
-<p>mar as trice</p>
+-->
 
 
-<hr/>
-<h5>mar a thuirt/chanas</h5>
-<p>as someone said/will say</p>
-<ul>
-  <li class="ex" data-slip="2895"></li>
-  <li class="ex" data-slip="2905"></li>
-  <li class="ex" data-slip="2904"></li>
-  <li class="ex" data-slip="2897"></li>
-</ul>
-<p>thuirt/ràdh but canas/canadh</p>
 
-<h5>1.4. Sentential <em>mar sin</em></h5>
-<p>so, therefore, for that reason</p>
-<ul>
-  <li class="ex" data-slip="2768"></li>
-  <li class="ex" data-slip="2769"></li>
-  <li class="ex" data-slip="2770"></li>
-  <li class="ex" data-slip="2771"></li>
-  <li class="ex" data-slip="2772"></li>
-  <li class="ex" data-slip="2773"></li>
-  <li class="ex" data-slip="2775"></li>
-  <li class="ex" data-slip="1776"></li>
-</ul>
-<p>is ann mar sin a ... ‘it was thus that ...’</p>
-<ul>
-  <li class="ex" data-slip="2774"></li>
-</ul>
+
 
 <?php
 require_once "../../includes/htmlFooter.php";
