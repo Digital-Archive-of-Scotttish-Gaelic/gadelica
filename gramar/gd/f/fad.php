cc8e28a6c7d9c330a80a6f62b9cbace863c6a927
conmaol <Mark.McConville@glasgow.ac.uk>
2020-12-03 23:49:38 +0000

    stuff

--- gramar/gd/f/fad.php
+++ gramar/gd/f/fad.php
@@ -2,18 +2,18 @@
 require_once "../../includes/htmlHeader.php";
 ?>
 <h1>fad</h1>
-<p><strong>fad</strong> is a noun with the basic meaning <span class="text-muted">‘length’</span>, derived from the adjective <strong>fada</strong> <span class="text-muted">‘long’</span>.</p>
-<p>It can also be used to introduce a time adverbial, as a kind of preposition or conjunction.</p>
+<p><strong>fad</strong> <span class="text-muted">‘length’</span> is an abstract noun, derived from the adjective <strong><a href="fada.php">fada</a></strong> <span class="text-muted">‘long’</span>.</p>
+<p>In additional to its straightforward nominal uses, <strong>fad</strong> can also be used to introduce a time adverbial, somewhat like a preposition or conjunction.</p>
 <hr/>
 <p>The main uses of <strong>fad</strong> are –</p>
 <ul>
-  <li>as noun meaning <span class="text-muted">'length'</span>, the quality of being <span class="text-muted">‘long’</span> <small><a data-toggle="modal" data-target="#noun" href="#">[more]</a></small></li>
-  <li>as a kind of preposition introducing a time adverbial (genitive case)
+  <li>as an abstract noun meaning <span class="text-muted">'length'</span>, the quality of being <span class="text-muted">‘long’</span> <small><a data-toggle="modal" data-target="#noun" href="#">[more]</a></small></li>
+  <li>as a pseudo-preposition introducing a time adverbial
     <ul>
       <li>governing an <em>indefinite</em> time noun, e.g. <mark>fad bliadhna</mark> <span class="text-muted">‘for a year’</span> <small><a data-toggle="modal" data-target="#preposition-indefinite" href="#">[more]</a></small></li>
       <li>governing a <em>definite</em> time noun, e.g. <mark>fad na bliadhna</mark> <span class="text-muted">‘all year (long)’</span> <small><a data-toggle="modal" data-target="#preposition-definite" href="#">[more]</a></small></li>
     </ul>
-  <li>as a kind of conjunction introducing a time adverbial, e.g. <mark>fhad ’s a bha mi a-muigh</mark> <span class="text-muted">‘while / as long as I was out’</span> <small><a data-toggle="modal" data-target="#conjunction" href="#">[more]</a></small></li>
+  <li>as a pseudo-conjunction introducing a time adverbial, e.g. <mark>fhad ’s a bha mi a-muigh</mark> <span class="text-muted">‘while / as long as I was out’</span> <small><a data-toggle="modal" data-target="#conjunction" href="#">[more]</a></small></li>
 </ul>
 
 <div class="modal fade" id="noun" tabindex="-1" role="dialog">
@@ -26,8 +26,8 @@ require_once "../../includes/htmlHeader.php";
         </button>
       </div>
       <div class="modal-body">
-        <p>The most straightforward use of <strong>fad</strong> is as a noun meaning <span class="text-muted">‘length’</span>, 
-        derived from the adjective <strong>fada</strong> <span class="text-muted">‘long’</span>. For example –</p>
+        <p>The most straightforward use of <strong>fad</strong> is as an abstract noun meaning <span class="text-muted">‘length’</span>, 
+        derived from the adjective <strong><a href="fada.php">fada</a></strong> <span class="text-muted">‘long’</span>. For example –</p>
         <ul>
           <li class="ex" data-slip="2938"></li>
         </ul>
@@ -43,21 +43,21 @@ require_once "../../includes/htmlHeader.php";
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel"><em>fad</em> as a preposition governing an indefinite time noun</h5>
+        <h5 class="modal-title" id="exampleModalLabel"><em>fad</em> as a pseudo-preposition – indefinite</h5>
         <button type="button" class="close" data-dismiss="modal">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
         <p><strong>fad</strong> can also be used as a kind of preposition introducing a time adverbial expressing the duration of a state or activity.
-        When <strong>fad</strong> governs an indefinite time noun, it means something like the English <span class="text-muted">‘for’</span>.</p>
+        When <strong>fad</strong> governs an <em>indefinite</em> time noun, it means something like the English <span class="text-muted">‘for’</span>.</p>
         <ul>
           <li class="ex" data-slip="2944"></li>
           <li class="ex" data-slip="2950"></li>
           <li class="ex" data-slip="2940"></li>
           <li class="ex" data-slip="2951"></li>
         </ul>
-        <p>See also: <strong>airson (bliadhna)</strong> <span class="text-muted">‘for (a year)’</span>.</p>
+        <p>See also: <strong><a href="../a/airson.php">airson</a></strong> <span class="text-muted">‘for’</span>.</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -70,14 +70,14 @@ require_once "../../includes/htmlHeader.php";
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel"><em>fad</em> as a preposition governing a definite time noun</h5>
+        <h5 class="modal-title" id="exampleModalLabel"><em>fad</em> as a pseudo-preposition – definite</h5>
         <button type="button" class="close" data-dismiss="modal">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <p>When preposition-like <strong>fad</strong> governs an definite time noun, 
-        it means something like the English <span class="text-muted">‘all ... (long)’</span>, <span class="text-muted">‘the whole’</span>, <span class="text-muted">‘throughout’</span>.</p>
+        <p>When the pseudo-preposition <strong>fad</strong> governs a <em>definite</em> time noun, 
+        it corresponds to the English <span class="text-muted">‘all ... (long)’</span>, <span class="text-muted">‘the whole’</span>, <span class="text-muted">‘throughout’</span>.</p>
         <ul>
           <li class="ex" data-slip="2947"></li>
           <li class="ex" data-slip="2935"></li>
@@ -98,25 +98,33 @@ require_once "../../includes/htmlHeader.php";
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel"><em>fad</em> as a conjunction</h5>
+        <h5 class="modal-title" id="exampleModalLabel"><em>fad</em> as a pseudo-conjunction</h5>
         <button type="button" class="close" data-dismiss="modal">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <p>fhad agus + positive relative clause</p>
-        <p>‘as long as’, ‘while’ (modifies non-bounded situation) –</p>
+        <p><strong>fad</strong> can also be used as a kind of conjunction, 
+        introducing a time adverbial expressing the time period over which some other situation extends, or within with an action happens.</p> 
+        <p>In this sense, <strong>fad</strong> is usually lenited and conjoined using a contracted version of 
+        <strong>agus</strong> <span class="text-muted">‘and’</span> with a relative clause, i.e. <strong>fhad ’s a (tha)</strong>.</p>
+        <p>When the pseudo-conjunction <strong>f(h)ad</strong> introduces a time adverbial which modifies a <em>non-bounded</em> state or activity, 
+          it corresponds to the English conjunctions <span class="text-muted">‘as long as’</span>, <span class="text-muted">‘while’</span>. 
+          The implication here is that the two situations are co-extensive, 
+          in that the superordinate state/activity only ceases when the subordinate state/activity comes to an end. For example –</p>
         <ul>
           <li class="ex" data-slip="2939"></li>
           <li class="ex" data-slip="2941"></li>
         </ul>
-        <p>‘while’ (modifies bounded situation) –</p>
+        <p>When the pseudo-conjunction <strong>f(h)ad</strong> introduces a time adverbial which modifies a <em>bounded</em> action or event, 
+          it corresponds to the English conjunction <span class="text-muted">‘while’</span>. 
+          The implication here is that the superordinate action/event takes place <em>within</em> the timespan of the subordinate state/activity. For example –</p>
         <ul>
           <li class="ex" data-slip="2943"></li>
           <li class="ex" data-slip="2945"></li>
           <li class="ex" data-slip="2948"></li>
         </ul>
-        <p>See also: <strong>nuair (a)</strong> <span class="text-muted">‘when’</span>.</p>
+        <p>See also: <strong><a href="../n/nuair.php">nuair</a></strong> <span class="text-muted">‘when’</span>.</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
