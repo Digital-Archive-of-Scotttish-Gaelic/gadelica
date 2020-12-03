5eb3eec646f86ad420c5f0a48d2a7ba8ecfe070d
conmaol <Mark.McConville@glasgow.ac.uk>
2020-12-03 00:13:23 +0000

    stuff

---
+++ gramar/gd/f/fad.php
@@ -0,0 +1,130 @@
+<?php
+require_once "../../includes/htmlHeader.php";
+?>
+<h1>fad</h1>
+<p><strong>fad</strong> is a noun with the basic meaning <span class="text-muted">‘length’</span>, derived from the adjective <strong>fada</strong> <span class="text-muted">‘long’</span>.</p>
+<p>It can also be used to introduce a time adverbial, as a kind of preposition or conjunction.</p>
+<hr/>
+<p>The main uses of <strong>fad</strong> are –</p>
+<ul>
+  <li>as noun meaning <span class="text-muted">'length'</span>, the quality of being <span class="text-muted">‘long’</span> <small><a data-toggle="modal" data-target="#noun" href="#">[more]</a></small></li>
+  <li>as a kind of preposition introducing a time adverbial (genitive case)
+    <ul>
+      <li>governing an <em>indefinite</em> time noun, e.g. <mark>fad bliadhna</mark> <span class="text-muted">‘for a year’</span> <small><a data-toggle="modal" data-target="#preposition-indefinite" href="#">[more]</a></small></li>
+      <li>governing a <em>definite</em> time noun, e.g. <mark>fad na bliadhna</mark> <span class="text-muted">‘all year (long)’</span> <small><a data-toggle="modal" data-target="#preposition-definite" href="#">[more]</a></small></li>
+    </ul>
+  <li>as a kind of conjunction introducing a time adverbial, e.g. <mark>fhad ’s a bha mi a-muigh</mark> <span class="text-muted">‘while / as long as I was out’</span> <small><a data-toggle="modal" data-target="#conjunction" href="#">[more]</a></small></li>
+</ul>
+
+<div class="modal fade" id="noun" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"><em>fad</em> as a noun meaning ‘length’</h5>
+        <button type="button" class="close" data-dismiss="modal">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>The most straightforward use of <strong>fad</strong> is as a noun meaning <span class="text-muted">‘length’</span>, 
+        derived from the adjective <strong>fada</strong> <span class="text-muted">‘long’</span>. For example –</p>
+        <ul>
+          <li class="ex" data-slip="2938"></li>
+        </ul>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="preposition-indefinite" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"><em>fad</em> as a preposition governing an indefinite time noun</h5>
+        <button type="button" class="close" data-dismiss="modal">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p><strong>fad</strong> can also be used as a kind of preposition introducing a time adverbial expressing the duration of a state or activity.
+        When <strong>fad</strong> governs an indefinite time noun, it means something like the English <span class="text-muted">‘for’</span>.</p>
+        <ul>
+          <li class="ex" data-slip="2944"></li>
+          <li class="ex" data-slip="2950"></li>
+          <li class="ex" data-slip="2940"></li>
+          <li class="ex" data-slip="2951"></li>
+        </ul>
+        <p>See also: <strong>airson (bliadhna)</strong> <span class="text-muted">‘for (a year)’</span>.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="preposition-definite" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"><em>fad</em> as a preposition governing a definite time noun</h5>
+        <button type="button" class="close" data-dismiss="modal">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>When preposition-like <strong>fad</strong> governs an definite time noun, 
+        it means something like the English <span class="text-muted">‘all ... (long)’</span>, <span class="text-muted">‘the whole’</span>, <span class="text-muted">‘throughout’</span>.</p>
+        <ul>
+          <li class="ex" data-slip="2947"></li>
+          <li class="ex" data-slip="2935"></li>
+          <li class="ex" data-slip="2949"></li>
+          <li class="ex" data-slip="2942"></li>
+          <li class="ex" data-slip="2946"></li>
+          <li class="ex" data-slip="2937"></li>
+        </ul>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="conjunction" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"><em>fad</em> as a conjunction</h5>
+        <button type="button" class="close" data-dismiss="modal">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>fhad agus + positive relative clause</p>
+        <p>‘as long as’, ‘while’ (modifies non-bounded situation) –</p>
+        <ul>
+          <li class="ex" data-slip="2939"></li>
+          <li class="ex" data-slip="2941"></li>
+        </ul>
+        <p>‘while’ (modifies bounded situation) –</p>
+        <ul>
+          <li class="ex" data-slip="2943"></li>
+          <li class="ex" data-slip="2945"></li>
+          <li class="ex" data-slip="2948"></li>
+        </ul>
+        <p>See also: <strong>nuair (a)</strong> <span class="text-muted">‘when’</span>.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+require_once "../../includes/htmlFooter.php";
+?>
