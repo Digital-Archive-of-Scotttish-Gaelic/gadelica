60ffec7246313cc5c696108e8144f94f8139bb9a
steviebarrett <mail@steviebarrett.com>
2021-01-15 10:39:12 +0000

    Reworked renameFiles to handle many directories

--- corpas/code/renameFiles.php
+++ corpas/code/renameFiles.php
@@ -1,20 +1,34 @@
+
 <?php
 
-$dir = "/var/www/html/dasg.arts.gla.ac.uk/www/gadelica/corpas/code/tmp";
+$dir = "/var/www/html/dasg.arts.gla.ac.uk/www/gadelica/corpas/scans/400_Gairm.bk";
 //$dir = "/Users/stephenbarrett/Sites/gadelica/corpas/code/tmp";
 
-$filenames = scandir($dir);
+renameFiles($dir);
 
-foreach ($filenames as $name) {
-	if (mb_substr($name, 0, 1) != '.') {
-		//rename files
-/*		$newName = $dir . "/" . "test_" . $name;
-		rename($dir . "/" . $name, $newName);
-*/
-		//remove prefix
-/*		$prefix = "test_";
-		$newName =  $dir . "/" . str_replace($prefix, "", $name);
-		rename($dir . "/" . $name, $newName);
-*/
+function renameFiles($rootDir)
+{
+	$directories = scandir($rootDir);
+	foreach ($directories as $dir) {
+		if (is_dir($dir)) {
+			if (mb_substr($dir, 0, 1) != '.') {
+				$dir = $rootDir . "/" . $dir;
+				$filenames = scandir($dir);
+				foreach ($filenames as $file) {
+
+					//rename files
+					/*
+						$newName = $dir . "/" . str_replace(' ', '_',  $file);
+						rename($dir . "/" . $file, $newName);
+					*/
+					//remove prefix
+					/*
+						$prefix = "400_Gairm_";
+						$newName =  $dir . "/" . str_replace($prefix, "", $file);
+						rename($dir . "/" . $file, $newName);
+					*/
+				}
+			}
+		}
 	}
 }
