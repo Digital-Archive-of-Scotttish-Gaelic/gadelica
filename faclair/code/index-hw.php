355fc198d42b9fa24e22ae75b1a9d3f03d44a2fc
conmaol <Mark.McConville@glasgow.ac.uk>
2021-02-15 15:15:12 +0000

    cosmetic stuff

--- faclair/code/index-hw.php
+++ faclair/code/index-hw.php
@@ -1,23 +1,23 @@
 <?php
 
-require_once 'includes/htmlHeader.php';
+require_once 'includes/htmlHeader2.php';
 
 $db = new models\database();
 
-$html = "<ul>";
+$html = "<div class=\"list-group list-group-flush\">";
 $query = <<<SQL
-	SELECT DISTINCT  `m-hw` ,  `m-pos` ,  `m-sub` 
-		FROM  `lexemes` 
+	SELECT DISTINCT  `m-hw` ,  `m-pos` ,  `m-sub`
+		FROM  `lexemes`
 		ORDER BY  `lexemes`.`m-hw` ASC
 SQL;
 $results = $db->fetch($query);
 foreach ($results as $result) {
 	$url = "viewMhw.php?mhw={$result["m-hw"]}&mpos={$result["m-pos"]}&msub={$result["m-sub"]}";
 	$html .= <<<HTML
-		<li><a href="{$url}">{$result["m-hw"]}</a></li>
+		<a href="{$url}" class="list-group-item list-group-item-action">{$result["m-hw"]}</a>
 HTML;
 }
-$html .= "</ul>";
+$html .= "</div>";
 echo $html;
 
-require_once 'includes/htmlFooter.php';
+require_once 'includes/htmlFooter2.php';
--- faclair/code/viewMhw.php
+++ faclair/code/viewMhw.php
@@ -1,15 +1,16 @@
 <?php
 
-require_once 'includes/htmlHeader.php';
+require_once 'includes/htmlHeader2.php';
 
 $html = <<<HTML
-	<h2>{$_GET["mhw"]} {$_GET["mpos"]}</h2>
+	<h1>{$_GET["mhw"]} <em>{$_GET["mpos"]}</em></h1>
+	<h5>Sources</h5>
 	<ul>
 HTML;
 $db = new \models\database();
 $sql = <<<SQL
-	SELECT `id`, `source`, `hw`, `pos` 
-		FROM `lexemes` 
+	SELECT `id`, `source`, `hw`, `pos`, `sub`
+		FROM `lexemes`
 		WHERE `m-hw` = :mhw
 		AND `m-pos` = :mpos
 		AND `m-sub` = :msub
@@ -17,19 +18,51 @@ SQL;
 $results = $db->fetch($sql, array(":mhw" => $_GET["mhw"], ":mpos" => $_GET["mpos"], ":msub" => $_GET["msub"]));
 foreach ($results as $result) {
 	$html .= <<<HTML
-		<li>{$result["id"]} {$result["source"]} {$result["hw"]} {$result["hw"]}</li>
+		<li>[{$result["source"]}] <strong>{$result["hw"]}</strong> <em>{$result["pos"]}</em> (#{$result["id"]})
+			<ul>
+				<!-- subquery to get forms ??
+SELECT  `form` ,  `morph` ,  `id`
+FROM  `forms`
+WHERE  `source` =  {$result["source"]}
+AND  `hw` =  {$result["hw"]}
+AND  `pos` =  {$result["pos"]}
+AND  `sub` =  {$result["sub"]}
+			  -->
+				<li>Forms:
+					<ul>
+						<li>[form 1]</li>
+						<li>[form 2]</li>
+						<li>[...]</li>
+					</ul>
+				</li>
+        <li>Translations:
+					<ul>
+						<li>[eng 1]</li>
+						<li>[eng 2]</li>
+						<li>[...]</li>
+					</ul>
+				</li>
+				<li>Notes:
+					<ul>
+						<li>[note 1]</li>
+						<li>[note 2]</li>
+						<li>[...]</li>
+					</ul>
+				</li>
+			</ul>
+		</li>
 HTML;
 }
 $html .= <<<HTML
     </ul>
-    <h3>Parts –</h3>
+    <h5>Parts</h5>
     <ul>
 HTML;
 
 //Parts
 $sql = <<<SQL
-	SELECT `m-p-hw`, `m-p-pos`, `m-p-sub` 
-		FROM `parts` 
+	SELECT `m-p-hw`, `m-p-pos`, `m-p-sub`
+		FROM `parts`
 		WHERE `m-hw` = :mhw
 		AND `m-pos` =  :mpos
 		AND `m-sub` =  :msub
@@ -43,14 +76,14 @@ HTML;
 }
 $html .= <<<HTML
 	</ul>
-	<h3>Compounds –</h3>
+	<h5>Compounds</h5>
 	<ul>
 HTML;
 
 //Compounds
 $sql = <<<SQL
-	SELECT `m-hw`, `m-pos`, `m-sub` 
-		FROM `parts` 
+	SELECT `m-hw`, `m-pos`, `m-sub`
+		FROM `parts`
 		WHERE `m-p-hw` = :mhw
 		AND `m-p-pos` = :mpos
 		AND `m-p-sub` = :msub
@@ -65,4 +98,4 @@ HTML;
 $html .= "</ul>";
 echo $html;
 
-require_once 'includes/htmlFooter.php';
+require_once 'includes/htmlFooter2.php';
