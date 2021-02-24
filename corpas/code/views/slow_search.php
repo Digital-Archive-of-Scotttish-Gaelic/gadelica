384686400ea569ca2679185849017367fbceb88a
steviebarrett <mail@steviebarrett.com>
2021-02-24 13:59:17 +0000

    Some basic introductory pagination code for slow_search

--- corpas/code/views/slow_search.php
+++ corpas/code/views/slow_search.php
@@ -35,11 +35,18 @@ HTML;
 HTML;
 			$path = '/var/www/html/dasg.arts.gla.ac.uk/www/gadelica/corpas/xml';
 			if (getcwd()=='/Users/stephenbarrett/Sites/gadelica/corpas/code') {
-				$path = '../../xml';
+				$path = '../xml';
 			}
 			else if (getcwd()=='/Users/mark/Sites/gadelica/corpas/code') {
 				$path = '../xml';
 			}
+
+			$html = <<<HTML
+				<table id="table" class="table">					
+					<tbody>
+HTML;
+
+			$count = 0;
 			$it = new \RecursiveDirectoryIterator($path);
 			foreach (new \RecursiveIteratorIterator($it) as $nextFile) {
 				if ($nextFile->getExtension()=='xml') {
@@ -54,10 +61,43 @@ HTML;
 					$xml = simplexml_load_file($nextFile);
 					$xml->registerXPathNamespace('dasg','https://dasg.ac.uk/corpus/');
 					foreach ($xml->xpath($xpath) as $nextWordId) {
-            echo $filename . ' ' . $nextWordId . '<br/>';
+						$count++;
+            $html .= <<<HTML
+							<tr>						
+								<th scope="row">{$count}</th>
+								<td>{$filename} {$nextWordId}</td>
+							</tr>
+HTML;
           }
         }
       }
+			$html .= <<<HTML
+					</tbody>
+				</table>
+				<div class="pagination"></div>
+
+				<script>
+					$(function () {
+					  	var items = $("table tr");
+							var numItems = items.length;
+							var perPage = 10;
+							items.slice(perPage).hide();
+							if(numItems != 0){
+								$(".pagination").pagination({
+									items: numItems,
+									itemsOnPage: perPage,
+									cssStyle: "light-theme",
+									onPageClick: function(pageNumber) { 
+										var showFrom = perPage * (pageNumber - 1);
+										var showTo = showFrom + perPage;
+										items.hide().slice(showFrom, showTo).show();
+									}
+								});
+							}
+					});
+				</script>
+HTML;
+			echo $html;
     }
 	}
 }
