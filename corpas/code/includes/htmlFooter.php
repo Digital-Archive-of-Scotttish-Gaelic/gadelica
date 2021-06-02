303a4a8f2413b0459ae32075ec370b5749f6db16
conmaol <Mark.McConville@glasgow.ac.uk>
2021-06-02 12:14:22 +0100

    moved xml etx

--- corpas/code/includes/htmlFooter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo <<<HTML
-    </div>
-    <script type="text/javascript" src="js/main.js"></script>
-</body>
-</html>
-HTML;
-
--- corpas/code/printSlip.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace controllers;
-
-require_once "includes/include.php";
-
-$controller = new slipprint();
-$controller->run("writePDF");
-
-
